3e413962be165e8bd6f5f961fe39a9b90d462530
Hitomi Kameyama <hb21a035@oecu.jp>
2024-05-28 15:24:00 +0900

    順番に応じた場所へ選択した素材の画像を表示するようにした

--- MakeBread/Assets/Scripts/ItemImageController.cs
+++ MakeBread/Assets/Scripts/ItemImageController.cs
@@ -16,19 +16,38 @@ public class ItemImageController : MonoBehaviour
     private Image _itemImage;
     private Sprite _itemSprite;
 
+    private RectTransform _rectTransform;
+
     // Start is called before the first frame update
     void Start()
     {
         _itemImage = this.gameObject.GetComponent<Image>();
         _itemSprite = Resources.Load<Sprite>("Images/" + itemname_ID);
+        _rectTransform = gameObject.GetComponent<RectTransform>();
+        
+
         if(_itemSprite == true)
         {
             _itemImage.sprite = _itemSprite;
         }
-        else
+        else { }
+
+        switch (num)
         {
-            return;
+            case 1:
+                _rectTransform.anchoredPosition = new Vector2(-720.0f, -270.0f);
+                return;
+            case 2:
+                _rectTransform.anchoredPosition = new Vector2(-240.0f, -270.0f);
+                return;
+            case 3:
+                _rectTransform.anchoredPosition = new Vector2(240.0f, -270.0f);
+                return;
+            case 4:
+                _rectTransform.anchoredPosition = new Vector2(720.0f, -270.0f);
+                return;
         }
+            
         
     }
 
