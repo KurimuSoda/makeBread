7194c2ff3532194fd14cff128717fd940758d142
Hitomi Kameyama <hb21a035@oecu.jp>
2024-05-29 23:44:34 +0900

    味の種類を格納した配列を作成。メインの味をDebug.Logにstringで表示するようにした。

--- MakeBread/Assets/Scripts/TasteManager.cs
+++ MakeBread/Assets/Scripts/TasteManager.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class TasteManager : MonoBehaviour
 {
     private int _mainTaste = 0;
+    /// <summary>選択した材料の味を選択した順番に格納する</summary>
     private int[] _tasteArray = new int[4];
+    private string[] _tasteStringArray = new string[5] {"甘い", "辛い", "酸っぱい", "しょっぱい", "苦い"};
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +21,7 @@ public class TasteManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Return))
         {
             _mainTaste = _tasteArray[3];
+            Debug.Log("main taste is ---> " + _mainTaste + " : " + _tasteStringArray[_mainTaste - 1]);
             //Debug.Log(_mainTaste);
             /*
             for(int i = 0; i < 4; i++)
