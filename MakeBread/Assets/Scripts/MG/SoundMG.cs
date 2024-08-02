589a85ff77f4f8b42b21ef9b7e92a40aa77937b1
Hitomi Kameyama <hb21a035@oecu.jp>
2024-08-02 14:32:31 +0900

    Revert "外部アセットを追加するためにコミットします。"
    This reverts commit dbd7909d1b45910351af718b2ec3298e2656d1a9.

--- MakeBread/Assets/Scripts/MG/SoundMG.cs
+++ MakeBread/Assets/Scripts/MG/SoundMG.cs
@@ -15,6 +15,11 @@ public class SoundMG : MonoBehaviour
         
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 
     public void BGMInit()
     {
--- MakeBread/Assets/Scripts/SetActiveResultBread.cs
+++ MakeBread/Assets/Scripts/SetActiveResultBread.cs
@@ -5,16 +5,8 @@ using UnityEngine;
 public class SetActiveResultBread : MonoBehaviour
 {
     //TODO change MG script Do!
-
-    /// <summary>
-    /// リザルトに表示するパンのID
-    /// </summary>
     [SerializeField]private string _firstItemID = "";
     private GameMG_new _gameMG;
-
-    /// <summary>
-    /// リザルトに表示するパンのオブジェクト。オブジェクトの名前は小文字＋番号
-    /// </summary>
     [SerializeField]private GameObject _resultBread;
 
     
@@ -24,12 +16,11 @@ public class SetActiveResultBread : MonoBehaviour
         _firstItemID = "";
 
         _gameMG = GameObject.FindWithTag("GameManager").GetComponent<GameMG_new>();
-        //GameMGからリザルトに表示するパンのIDを受け取る
         _firstItemID = _gameMG.SendItemID();
 
         //_firstItemID = "spi_01";  //for Debug
 
-        if(_firstItemID == "")  //IDと同じ名前のオブジェクトが見つからなかったとき
+        if(_firstItemID == "")
         {
             _resultBread = transform.Find("FishBread").gameObject;
             //Debug.Log("result bread dont find...");
