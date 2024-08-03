9361171f96b5aabd9b9809c024a62ffb07bfdffc
Hitomi Kameyama <hb21a035@oecu.jp>
2024-08-04 01:05:25 +0900

    BGMがシングルトンの対象から外れたのでSceneスタート時に初期化するように変更しました。

--- MakeBread/Assets/Scripts/MG/SoundMG.cs
+++ MakeBread/Assets/Scripts/MG/SoundMG.cs
@@ -13,6 +13,7 @@ public class SoundMG : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        BGMInit();
         ChangeBGM(SceneManager.GetActiveScene().name);
     }
 
